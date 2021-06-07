eb4d916213466ce581711f8fe9eafa0c741ad9c8
Aljosha Judmayer <kernoelpanic@3-volution.net>
2021-06-07 13:16:40 +0000

    Added helper script to show version and help of dockerized geth

---
+++ geth_help.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#
+# Run with docker:
+# $ bash geth_version.sh
+
+
+# run container without network
+# just to generate and manage keys
+docker run \
+	--mount type=bind,source=$(pwd),target=/smartenv \
+	-it smartenv:latest \
+		geth \
+		--nodiscover \
+		--maxpeers 0 \
+		--ipcdisable \
+		--verbosity 6  \
+		help 
+
